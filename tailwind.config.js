c8a47f104161fe14c733720143ef67be251c77d8
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2025-10-30 17:03:16 +0100

    feat:cover

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // ...
+  theme: {
+    colors: {
+      //'blue': '#1fb6ff',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+      fruktur: ['Fruktur', 'cursive'],
+    }
+  }
+}
