d66a65ed8040fcd16b2e139c3d54d69d7d9c9520
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2025-11-07 13:50:17 +0100

    fix:astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+    },
+  },
 });
--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -4,7 +4,7 @@ import type { TypedPocketBase } from "./pocketbase-types";
 const path =
   import.meta.env.MODE === "development"
     ? "http://127.0.0.1:8090"
-    : "http://localhost:8089";
+    : "https://portfolio.mathis-guellati.fr";
 
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
