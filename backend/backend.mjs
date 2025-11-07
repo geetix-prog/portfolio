eda554041046e6245231fd088d6264be58f18c5c
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2025-11-07 13:10:34 +0100

    fix:pocketbase-route

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,5 +1,4 @@
-import PocketBase from "pocketbase";
-const pb = new PocketBase("http://127.0.0.1:8090/");
+import pb from "../src/utils/pb.ts";
 
 export async function allCompetences() {
   try {
--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,10 +1,11 @@
 import PocketBase from "pocketbase";
-import type { TypedPocketBase } from "./pocketbase-types";
 
-const path =
-  import.meta.env.MODE === "development"
-    ? "http://127.0.0.1:8090"
-    : "http://localhost:8089";
+// Use local PocketBase when developing, production URL when deployed
+const DEV_URL = "http://127.0.0.1:8090";
+const PROD_URL = "https://portfolio.mathis-guellati.fr/_/";
 
-const pb = new PocketBase(path) as TypedPocketBase;
+const baseURL = import.meta.env.MODE === "development" ? DEV_URL : PROD_URL;
+
+const pb = new PocketBase(baseURL);
+
 export default pb;
