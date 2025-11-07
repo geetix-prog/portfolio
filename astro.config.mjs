4122d2dafc127b30e29ebb599c931485e79a01cf
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2025-11-07 13:00:08 +0100

    install node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
