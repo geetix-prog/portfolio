e6e44daf083dc9d89acc440f050c1e5743c9c6fc
Mathis <mathis.guellati@gmail.com>
2025-11-19 21:42:48 +0100

    feat:seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://portfolio.mathis-guellati.fr",
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
     server: {
